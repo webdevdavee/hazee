6b104f932d64000ac803153ccb4e47ef234b0398
SirDaechel <anayookpala26@gmail.com>
2024-08-06 15:28:42 +0100

    make navbar sticky

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -4,27 +4,38 @@ import Button from "../ui/Button";
 
 const Navbar = () => {
   return (
-    <section className="container">
-      <nav className="flex items-center justify-between">
-        <div className="flex gap-10 items-center">
-          <Link href="/" className="text-white text-2xl">
-            Hazee.
-          </Link>
-          <Searchbar />
-        </div>
-        <ul className="flex gap-5 items-center">
-          <Link href="/" className="text-[gray] font-medium">
-            Explore
-          </Link>
-          <Link href="/" className="text-[gray] font-medium">
-            Collections
-          </Link>
-          <Link href="/" className="text-[gray] font-medium">
-            Gallery
-          </Link>
-          <Button text="Connect wallet" style="bg-primary font-medium" />
-        </ul>
-      </nav>
+    <section className="sticky top-0 z-50">
+      <div className="backdrop-blur-md bg-base/70">
+        <nav className="container mx-auto flex items-center justify-between py-4">
+          <div className="flex gap-10 items-center">
+            <Link href="/" className="text-white text-2xl">
+              Hazee.
+            </Link>
+            <Searchbar />
+          </div>
+          <ul className="flex gap-5 items-center">
+            <Link
+              href="/"
+              className="text-[gray] font-medium hover:text-white transition-colors"
+            >
+              Explore
+            </Link>
+            <Link
+              href="/"
+              className="text-[gray] font-medium hover:text-white transition-colors"
+            >
+              Collections
+            </Link>
+            <Link
+              href="/"
+              className="text-[gray] font-medium hover:text-white transition-colors"
+            >
+              Gallery
+            </Link>
+            <Button text="Connect wallet" style="bg-primary font-medium" />
+          </ul>
+        </nav>
+      </div>
     </section>
   );
 };
