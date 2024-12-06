c8cd647c0a0b69e915f71aaf41f697980668195c
webdevdavee <anayookpala26@gmail.com>
2024-12-06 20:43:18 +0100

    fix bg colour - 3

--- components/layout/MobileNav.tsx
+++ components/layout/MobileNav.tsx
@@ -24,7 +24,7 @@ const MobileNav: React.FC<Props> = ({
 
   return (
     <nav className="w-full px-6 hidden m:block py-4 fixed top-16 h-screen bg-base shadow-md xl:block">
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-8">
         <NavbarSearch />
 
         <div>
@@ -40,7 +40,7 @@ const MobileNav: React.FC<Props> = ({
             />
           </div>
           {isExploreOpen && (
-            <div className="mt-3 flex flex-col gap-3 pl-3">
+            <div className="mt-3 flex flex-col gap-5 pl-3">
               <Link
                 href="/explore/nfts"
                 className="text-sm text-slate-200 flex items-center gap-3"
@@ -82,7 +82,7 @@ const MobileNav: React.FC<Props> = ({
             />
           </div>
           {isCreateOpen && (
-            <div className="mt-3 flex flex-col gap-3 pl-3">
+            <div className="mt-3 flex flex-col gap-5 pl-3">
               <Link
                 href="/studio/nft"
                 className="text-sm text-slate-200 flex items-center gap-3"
@@ -111,7 +111,7 @@ const MobileNav: React.FC<Props> = ({
               onclick={handleOpenModal}
             />
           ) : (
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-3 mt-4">
               <Link href={`/creator/${walletAddress}`}>
                 <Button
                   text={truncatedAddress as string}
